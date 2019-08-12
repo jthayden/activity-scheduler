31c0a6bc1af52c1566a54cedb7c9aa1e08125dd8
John Hayden <jthayden10@me.com>
2019-08-12 17:43:19 -0400

    Added URLs

--- scheduler_app/urls.py
+++ scheduler_app/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register('golf-courses', views.GolfCourseView)
+router.register('tee-times', views.TeeTimeBookingView)
+
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
