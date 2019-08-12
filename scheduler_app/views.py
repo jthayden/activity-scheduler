2d160509a585ef87c9d1ef76fc60fad81bd8c4f4
John Hayden <jthayden10@me.com>
2019-08-12 17:19:23 -0400

    Added views for golf course and tee time booking

--- scheduler_app/views.py
+++ scheduler_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import GolfCourseSerializer, TeeTimeBookingSerializer
+from .models import GolfCourse, TeeTimeBooking
 
 # Create your views here.
+class GolfCourseView(viewsets.ModelViewSet):
+    queryset = GolfCourse.objects.all()
+    serializer_class = GolfCourseSerializer
+
+class TeeTimeBookingView(viewsets.ModelViewSet):
+    queryset = TeeTimeBooking.objects.all()
+    serializer_class = TeeTimeBookingSerializer
