72c4526697e764253c6148d410d18eb5ee7c64fa
John Hayden <jthayden10@me.com>
2019-08-12 14:11:45 -0400

    Corrected settings.py

--- scheduler_project/settings.py
+++ scheduler_project/settings.py
@@ -52,7 +52,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'act_scheduler__project.urls'
+ROOT_URLCONF = 'scheduler__project.urls'
 
 TEMPLATES = [
     {
@@ -70,7 +70,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'act_scheduler__project.wsgi.application'
+WSGI_APPLICATION = 'scheduler__project.wsgi.application'
 
 
 # Database
