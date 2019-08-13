979a37e2e8872fd24cb0026b554c8ae695c882cf
John Hayden <jthayden10@me.com>
2019-08-13 10:38:45 -0400

    Registered new models for django admin

--- scheduler_app/admin.py
+++ scheduler_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import GolfCourse, TeeTimeBooking
+from .models import GolfCourse, TeeTimeBooking, GolfLessonBooking, TennisComplex, CourtBooking, TennisLessonBooking
 
 # Register your models here.
-admin.site.register([GolfCourse, TeeTimeBooking])
+admin.site.register([GolfCourse, TeeTimeBooking, GolfLessonBooking, TennisComplex, CourtBooking, TennisLessonBooking])
