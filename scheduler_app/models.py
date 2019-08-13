535eb22c377cc0c7187af252817d7a574d9bd75f
John Hayden <jthayden10@me.com>
2019-08-13 09:33:02 -0400

    Added models for golf lesson booking, tennis complex, court booking, and tennis lesson booking

--- scheduler_app/models.py
+++ scheduler_app/models.py
@@ -17,4 +17,40 @@ class TeeTimeBooking(models.Model):
     course = models.ForeignKey(GolfCourse, on_delete=models.CASCADE, related_name='tee_time_bookings')
 
     def __str__(self):
-        return self.title
+        return self.name
+
+class LessonBooking(models.Model):
+    name = models. CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    pro = models.CharField(max_length=255)
+    course = models.ForeignKey(GolfCourse, on_delete=models.CASCADE, related_name='lesson_bookings')
+    
+    def __str__(self):
+        return self.name   
+
+class TennisComplex(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.CharField(max_length=700)
+    photo_url = models.CharField(max_length=400)
+
+    def __str__(self):
+        return self.name
+
+class CourtBooking(models.Model):
+    name = models.CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    court_type = models.CharField(max_length=255)
+    guests = models.CharField(max_lenght=255)
+    tennis_complex = models.ForeignKey(TennisComplex, on_delete=models.CASCADE, related_name='court_bookings') 
+
+    def __str__(self):
+        return self.name
+
+class TennisLessonBooking(models.Model):
+    name = models. CharField(max_length=255)
+    time = models.CharField(max_length=255)
+    pro = models.CharField(max_length=255)
+    tennis_complex = models.ForeignKey(TennisComplex, on_delete=models.CASCADE, related_name='tennis_lesson_bookings')
+    
+    def __str__(self):
+        return self.name 
