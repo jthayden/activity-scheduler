210ed946ea1619e9a0122e4e4b88fba868de36f2
John Hayden <jthayden10@me.com>
2019-08-13 10:11:20 -0400

    Added views for golf lessons, tennis complex, court booking, tennis lessons

--- scheduler_app/views.py
+++ scheduler_app/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import viewsets
-from .serializers import GolfCourseSerializer, TeeTimeBookingSerializer
-from .models import GolfCourse, TeeTimeBooking
+from .serializers import GolfCourseSerializer, TeeTimeBookingSerializer, GolfLessonBookingSerializer, TennisComplexSerializer, CourtBookingSerializer, TennisLessonBookingSerializer
+from .models import GolfCourse, TeeTimeBooking, GolfLessonBooking, TennisComplex, CourtBooking, TennisLessonBooking
 
 # Create your views here.
 class GolfCourseView(viewsets.ModelViewSet):
@@ -11,3 +11,19 @@ class GolfCourseView(viewsets.ModelViewSet):
 class TeeTimeBookingView(viewsets.ModelViewSet):
     queryset = TeeTimeBooking.objects.all()
     serializer_class = TeeTimeBookingSerializer
+
+class GolfLessonBookingView(viewsets.ModelViewSet):
+    queryset = GolfLessonBooking.objects.all()
+    serializer_class = GolfLessonBookingSerializer
+
+class TennisComplexView(viewsets.ModelViewSet):
+    queryset = TennisComplex.objects.all()
+    serializer_class = TennisComplexSerializer
+
+class CourtBookingView(viewsets.ModelViewSet):
+    queryset = CourtBooking.objects.all()
+    serializer_class = CourtBookingSerializer
+
+class TennisLessonBookingView(viewsets.ModelViewSet):
+    queryset = TennisLessonBooking.objects.all()
+    serializer_class = TennisLessonBookingSerializer
