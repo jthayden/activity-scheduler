5e1efb9f938a8ad89549dde6fdde5ef7da0169a2
John Hayden <jthayden10@me.com>
2019-08-13 13:05:23 -0400

    Corrected typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import GolfCourseList from './components/GolfCourseList'
-import GolfCourse from './components/GolfCourse'
+// import GolfCourse from './components/GolfCourse'
 import './App.css'
 
 class App extends Component {
