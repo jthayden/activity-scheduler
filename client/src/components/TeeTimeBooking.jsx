1d615f5cd4079f057d4f7b0f0bba999b694dde3e
John Hayden <jthayden10@me.com>
2019-08-15 15:28:52 -0400

    Added buttons for edit tee time and delete tee time.  Both working.

--- client/src/components/TeeTimeBooking.jsx
+++ client/src/components/TeeTimeBooking.jsx
@@ -39,7 +39,7 @@ export default class TeeTimeBooking extends Component {
     handleSubmit = (evt) => {
         evt.preventDefault()
 
-        axios.post(`/api/v1/teetimes/${this.state.tee_time_booking.id}/`, this.state.tee_time_booking)
+        axios.put(`/api/v1/teetimes/${this.state.tee_time_booking.id}/`, this.state.tee_time_booking)
             .then(() => {
                 this.setState({
                     redirectToHome:true
@@ -109,6 +109,9 @@ export default class TeeTimeBooking extends Component {
                 <h5>Guests: {this.state.tee_time_booking.guests}</h5>
                 <h5>Carts: {this.state.tee_time_booking.carts}</h5>
                 {/* <h5>Course: {this.state.tee_time_booking.course}</h5> */}
+
+                <button onClick={this.toggleEditTeeTimeBookingForm}>Edit Tee Time</button>
+                <button onClick={this.handleDeleteTeeTimeBooking}>Delete Tee Time</button>
                 </div>
                 }
 
