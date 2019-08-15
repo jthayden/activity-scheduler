aebf1c760f841338c38a1dc4c95b58117bdcd6ff
John Hayden <jthayden10@me.com>
2019-08-15 14:53:12 -0400

    Mapped over tee time booking info to single TeeTimeBooking page

--- client/src/components/TeeTimeBooking.jsx
+++ client/src/components/TeeTimeBooking.jsx
@@ -4,18 +4,27 @@ import { Redirect, Link } from 'react-router-dom'
 
 export default class TeeTimeBooking extends Component {
     state = {
-        tee_time_booking: [],
+        tee_time_booking: {},
         redirectToHome: false
     }
 
     componentDidMount() {
         axios.get(`/api/v1/teetimes/${this.props.match.params.id}/`)
+        .then(res => {
+            this.setState({
+                tee_time_booking: res.data
+            })
+        })
     }
 
     render() {
         return (
             <div>
-                
+                <h5>Name: {this.state.tee_time_booking.name}</h5>
+                <h5>Time: {this.state.tee_time_booking.time}</h5>
+                <h5>Guests: {this.state.tee_time_booking.guests}</h5>
+                <h5>Carts: {this.state.tee_time_booking.carts}</h5>
+                {/* <h5>Course: {this.state.tee_time_booking.course}</h5> */}
             </div>
         )
     }
