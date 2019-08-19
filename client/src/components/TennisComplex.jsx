3fefe57e76d37a68fadd634755c3c6939c711c6c
John Hayden <jthayden10@me.com>
2019-08-19 17:58:25 -0400

    Added expansion panels for court bookings and tennis lessons.  Tested and working!

--- client/src/components/TennisComplex.jsx
+++ client/src/components/TennisComplex.jsx
@@ -1,6 +1,13 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Redirect, Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import '../index.css'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default class TennisComplex extends Component {
     state = {
@@ -35,17 +42,60 @@ export default class TennisComplex extends Component {
     render() {
         let courtBookingList = this.state.tennis_complex.court_bookings.map((court_booking) => {
             return (
-                <Link to={`/courtbookings/${court_booking.id}/`}>
-                    <p>{court_booking.name}{court_booking.time}{court_booking.pro}{court_booking.guests}{court_booking.court_type}{court_booking.tennis_complex}</p>
-                </Link>
+                <div className='root'>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography className='heading'>{court_booking.time}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                    <Typography>
+                        Member: {court_booking.name} 
+                        <br/>
+                        Guest(s): {court_booking.guests}
+                        <br/>
+                        Court Type: {court_booking.court_type}
+                        <Link to={`/courtbookings/${court_booking.id}/`}><Button className='info-button' size="small"> Details</Button></Link>
+                    </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+                
+                // <Link to={`/courtbookings/${court_booking.id}/`}>
+                //     <p>{court_booking.name}{court_booking.time}{court_booking.pro}{court_booking.guests}{court_booking.court_type}{court_booking.tennis_complex}</p>
+                // </Link>
             )
         })
 
         let tennisLessonBookingList = this.state.tennis_complex.tennis_lesson_bookings.map((tennis_lesson_booking) => {
             return (
-                <Link to={`/tennislessons/${tennis_lesson_booking.id}/`}>
-                    <p>{tennis_lesson_booking.name}{tennis_lesson_booking.time}{tennis_lesson_booking.pro}{tennis_lesson_booking.tennis_complex}</p>
-                </Link>
+                <div className='root'>
+                <ExpansionPanel>
+                    <ExpansionPanelSummary
+                    expandIcon={<ExpandMoreIcon />}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                    >
+                    <Typography className='heading'>{tennis_lesson_booking.time}</Typography>
+                    </ExpansionPanelSummary>
+                    <ExpansionPanelDetails>
+                    <Typography>
+                        Member: {tennis_lesson_booking.name} 
+                        <br/>
+                        Pro: {tennis_lesson_booking.pro}
+                        <Link to={`/tennislessons/${tennis_lesson_booking.id}/`}><Button className='info-button' size="small"> Details</Button></Link>
+                    </Typography>
+                    </ExpansionPanelDetails>
+                </ExpansionPanel>
+            </div>
+
+
+                // <Link to={`/tennislessons/${tennis_lesson_booking.id}/`}>
+                //     <p>{tennis_lesson_booking.name}{tennis_lesson_booking.time}{tennis_lesson_booking.pro}{tennis_lesson_booking.tennis_complex}</p>
+                // </Link>
             )
         })
 
