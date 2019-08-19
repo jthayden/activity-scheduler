a6e2126e239d7fae87664a00fe92c9177c9cd158
John Hayden <jthayden10@me.com>
2019-08-19 17:50:00 -0400

    Added expansion panels for tee times and golf lessons. Tested and working!

--- client/src/components/GolfCourse.jsx
+++ client/src/components/GolfCourse.jsx
@@ -1,6 +1,16 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Redirect, Link } from 'react-router-dom'
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import '../index.css'
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 
 export default class GolfCourse extends Component {
     state = {
@@ -40,17 +50,60 @@ export default class GolfCourse extends Component {
     render() {
         let teeTimeBookingList = this.state.golf_course.tee_time_bookings.map((tee_time_booking) => {
             return (
-                <Link to={`/teetimes/${tee_time_booking.id}/`}>    
-                <p>{tee_time_booking.name}{tee_time_booking.time}{tee_time_booking.guests}{tee_time_booking.carts}{tee_time_booking.course}</p>
-                </Link>
+                <div>
+                <div className='root'>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                        <Typography className='heading'>{tee_time_booking.time}</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                        <Typography>
+                            Member: {tee_time_booking.name} 
+                            <br/>
+                            Guest(s): {tee_time_booking.guests}
+                            <br/>
+                            Cart(s): {tee_time_booking.carts}
+                            <Link to={`/teetimes/${tee_time_booking.id}/`}><Button className='info-button' size="small"> Details</Button></Link>
+                        </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                </div>
+                    {/* {tee_time_booking.course} */}
+
+                
+                {/* <Link to={`/teetimes/${tee_time_booking.id}/`}>     */}
+                {/* <p>{tee_time_booking.name}{tee_time_booking.time}{tee_time_booking.guests}{tee_time_booking.carts}{tee_time_booking.course}</p> */}
+                
+                </div>
             )
         })
 
         let golfLessonBookingList = this.state.golf_course.golf_lesson_bookings.map((golf_lesson_booking) => {
             return (
-                <Link to={`/golflessons/${golf_lesson_booking.id}/`}>    
-                <p>{golf_lesson_booking.name}{golf_lesson_booking.time}{golf_lesson_booking.pro}{golf_lesson_booking.course}</p>
-                </Link>
+                <div className='root'>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                        <Typography className='heading'>{golf_lesson_booking.time}</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                        <Typography>
+                            Member: {golf_lesson_booking.name} 
+                            <br/>
+                            Pro: {golf_lesson_booking.pro}
+                            <Link to={`/golflessons/${golf_lesson_booking.id}/`}><Button className='info-button' size="small"> Details</Button></Link>
+                        </Typography>
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    {/* {golf_lesson_booking.course} */}
+                </div>
             )
         })
 
