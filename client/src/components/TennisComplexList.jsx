4f3f932a6d7070f1192847e4a18d43bc66937d25
John Hayden <jthayden10@me.com>
2019-08-20 10:45:08 -0400

    Added tennis complex navbar

--- client/src/components/TennisComplexList.jsx
+++ client/src/components/TennisComplexList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button'
 
 export default class TennisComplexList extends Component {
     state = {
@@ -25,7 +26,7 @@ getAllTennisComplexes = () => {
         let tennisComplexList = this.state.tennis_complexes.map((tennis_complex) => {
             return (
                 <Link to={`/tenniscomplexes/${tennis_complex.id}`}>
-                <div>
+                <div id={tennis_complex.name}>
                     <img src={tennis_complex.photo_url} alt={tennis_complex.name} />
                     <h2>{tennis_complex.name}</h2>
                     <p>{tennis_complex.description}</p>
@@ -35,6 +36,9 @@ getAllTennisComplexes = () => {
         })
         return (
             <div>
+                <Button href="#Lake Club Tennis Center" className=''>The Lake Club Tennis Center</Button>
+                <Button href="#The Landing Complex">The Landing Complex</Button>
+
                 {tennisComplexList}
                 <Link to={'/tenniscomplexes/create'}>Add Tennis Complex</Link>
             </div>
