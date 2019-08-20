e6329087232aee285ad109ed64758bd7716dd25c
John Hayden <jthayden10@me.com>
2019-08-20 18:57:18 -0400

    Redid map of golf courses

--- client/src/components/GolfCourseList.jsx
+++ client/src/components/GolfCourseList.jsx
@@ -2,6 +2,8 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import Button from '@material-ui/core/Button';
+import ButtonBase from '@material-ui/core/ButtonBase';
+import Typography from '@material-ui/core/Typography';
 
 export default class GolfCourseList extends Component {
     state = {
@@ -26,13 +28,38 @@ export default class GolfCourseList extends Component {
     render() {
         let golfCourseList = this.state.golf_courses.map((golf_course) => {
             return (
-                <Link to={`/golfcourses/${golf_course.id}`}>
+                <div>
+                
+                <ButtonBase focusRipple key={golf_course.name} className='image'>
+                    {/* <span className='imageSrc'/> */}
+                    <img src={golf_course.photo_url} alt={golf_course.name}/>
+                    {/* <span className='imageBackdrop'/> */}
+                    <span className='imageButton'>
+                    <Link to={`/golfcourses/${golf_course.id}`}>
+                        <Typography
+                            component='span'
+                            // variant='subtitle1'
+                            // color='white'
+                            className='imageTitle'
+                            >{golf_course.name}
+                            {/* <span className='imageMarked' /> */}
+                            </Typography>
+                            </Link>
+                            </span>
+                </ButtonBase>
+                <p>{golf_course.description}</p>
+                
+                <hr />
+                
+                
+                {/* <Link to={`/golfcourses/${golf_course.id}`}>
                 <div id={golf_course.name}>
                     <img src={golf_course.photo_url} alt={golf_course.name} />
                     <h2>{golf_course.name}</h2>
                     <p>{golf_course.description}</p>
                 </div>
-                </Link>
+                </Link> */}
+                </div>
             )
         })
         return (
