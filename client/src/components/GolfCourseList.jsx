12d230a93db946fd116037dc42814b520a82a391
John Hayden <jthayden10@me.com>
2019-08-20 10:24:36 -0400

    Add golf course navbar

--- client/src/components/GolfCourseList.jsx
+++ client/src/components/GolfCourseList.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
+import Button from '@material-ui/core/Button';
 
 export default class GolfCourseList extends Component {
     state = {
@@ -26,7 +27,7 @@ export default class GolfCourseList extends Component {
         let golfCourseList = this.state.golf_courses.map((golf_course) => {
             return (
                 <Link to={`/golfcourses/${golf_course.id}`}>
-                <div>
+                <div id={golf_course.name}>
                     <img src={golf_course.photo_url} alt={golf_course.name} />
                     <h2>{golf_course.name}</h2>
                     <p>{golf_course.description}</p>
@@ -36,6 +37,13 @@ export default class GolfCourseList extends Component {
         })
         return (
             <div>
+                <Button href="#The Preserve" className=''>The Preserve</Button>
+                <Button href="#The Oconee" className=''>The Oconee</Button>
+                <Button href="#Great Waters" className=''>Great Waters</Button>
+                <Button href="#The National" className=''>The National</Button>
+                <Button href="#Creek Club" className=''> Creek Club</Button>
+                <Button href="#The Landing" className=''>The Landing</Button>
+
                 {golfCourseList}
                 <Link to={'/golfcourses/create'}>Add Golf Course</Link>
             </div>
